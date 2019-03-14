917a8beabc431cc6b888c0964325a1cda9d118f3
Christina Harlow <cmharlow@gmail.com>
2019-03-14 15:54:37 -0400

    adding molecule testing

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,4 +1,5 @@
 import os
+import pytest
 
 import testinfra.utils.ansible_runner
 
@@ -12,3 +13,33 @@ def test_hosts_file(host):
     assert f.exists
     assert f.user == 'root'
     assert f.group == 'root'
+
+
+# Firewalld Exists
+@pytest.mark.parametrize('pkg', ['firewalld'])
+def test_pkg(host, pkg):
+    package = host.package(pkg)
+    assert package.is_installed
+
+
+# Firewalld is Running
+@pytest.mark.parametrize('svc', ['firewalld'])
+def test_svc(host, svc):
+    service = host.service(svc)
+    assert service.is_running
+    assert service.is_enabled
+
+
+# Firewall rules for Public Zone and Enabled Status are present
+@pytest.mark.parametrize('file, content', [
+    ("/etc/firewalld/zones/public.xml", "<service name=\"http\"/>"),
+    ("/etc/firewalld/zones/public.xml", "<service name=\"https\"/>"),
+    ("/etc/firewalld/zones/public.xml",
+     "<port protocol=\"tcp\" port=\"8080\"/>"),
+    ("/etc/firewalld/zones/public.xml",
+     "<forward-port to-port=\"8080\" protocol=\"tcp\" port=\"80\"/>")
+    ])
+def test_public_services(host, file, content):
+    file = host.file(file)
+    assert file.exists
+    assert file.contains(content)
